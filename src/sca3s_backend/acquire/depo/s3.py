20479ef2177835d716338444519fc3c792ccd33a
James Webb <james@ultra-horizon.com>
2019-08-22 14:19:36 +0100

    Add untested framework for zipping of job traces.

--- src/sca3s_backend/acquire/depo/s3.py
+++ src/sca3s_backend/acquire/depo/s3.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2018 SCARV project <info@scarv.org>
 #
-# Use of this source code is restricted per the MIT license, a copy of which 
-# can be found at https://opensource.org/licenses/MIT (or should be included 
+# Use of this source code is restricted per the MIT license, a copy of which
+# can be found at https://opensource.org/licenses/MIT (or should be included
 # as LICENSE.txt within the associated archive or repository).
 
+import tempfile, zipfile
 import sca3s_backend as be
 import sca3s_spec    as spec
 
@@ -21,7 +22,7 @@ class DepoImp( depo.DepoAbs ) :
     super().__init__( job )
 
     self.access_key_id = be.share.sys.conf.get( 'creds', section = 'security' ).get( 'access-key-id', section = 's3' )
-    self.access_key    = be.share.sys.conf.get( 'creds', section = 'security' ).get( 'access-key',    section = 's3' ) 
+    self.access_key    = be.share.sys.conf.get( 'creds', section = 'security' ).get( 'access-key',    section = 's3' )
 
     self.region_id     =       self.depo_spec.get( 'region-id'   )
     self.bucket_id     =       self.depo_spec.get( 'bucket-id'   )
@@ -33,23 +34,18 @@ class DepoImp( depo.DepoAbs ) :
     resource = session.resource( 's3' )
     bucket   = resource.Bucket( self.bucket_id )
 
-    def copy( src, quiet = False ) :
-      dst = os.path.join( self.job.user_id, self.job.id, os.path.relpath( src, start = self.job.path ) )
+    def zip_directory(path, zipf):
+      for root, dirs, files in os.walk(path):
+        for file in files:
+          zipf.write(os.path.join(root, file))
 
-      if ( not quiet ) :
-        self.job.log.debug( '[src=%s] --> [dst=%s]' % ( os.path.relpath( src, start = self.job.path ), dst ) )
+    with tempfile.TemporaryFile() as tmp:
+      with zipfile.ZipFile(tmp, 'w', zipfile.ZIP_DEFLATED) as archive:
+        zip_directory(self.job.path, archive)
+      # Reset file pointer and return zip data (don't think this is required, but left for raw read if needed...)
+      # tmp.seek(0)
+      # I'll let you deal with the log messages...
+      # self.job.log.debug('[src=%s] --> [dst=%s]' % (os.path.relpath(tmp.name, start=self.job.path), dst))
+      bucket.upload_file(tmp, self.job.conf['id'][:10] + '.zip')
 
-      with open( src, 'rb' ) as data :
-        r = bucket.put_object( Key = dst, Body = data )
-
-        if ( ( self.verify ) and ( r.e_tag.strip( '"' ) != be.share.util.MD5( src ) ) ) :
-          raise Exception()
-
-    log = os.path.join( self.job.path, 'job.log' )
-  
-    for ( dir, dirs, files ) in os.walk( self.job.path ) :
-      for src in [ os.path.join( dir, file ) for file in files ] :
-        if ( src != log ) :  
-          copy( src, quiet = False )
-
-    self.job.log.shutdown() ; copy( log, quiet = True )
+    self.job.log.shutdown()
