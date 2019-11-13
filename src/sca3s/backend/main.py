c4d8658df88d343f9b3cccb015866ace924f35e1
Dan Page <dan@phoo.org>
2019-11-13 10:03:30 +0000

    refactor

--- src/sca3s/backend/main.py
+++ src/sca3s/backend/main.py
@@ -4,8 +4,8 @@
 # can be found at https://opensource.org/licenses/MIT (or should be included 
 # as LICENSE.txt within the associated archive or repository).
 
-from sca3s import backend as be
-from sca3s import share   as share
+from sca3s import backend    as sca3s_be
+from sca3s import middleware as sca3s_mw
 
 import importlib, os, shutil, signal, tempfile, time
 
@@ -19,10 +19,10 @@ def process( manifest ) :
   id = None ; result = STATUS_SUCCESS
 
   try :
-    be.share.sys.log.info( 'validating job' )
+    sca3s_be.share.sys.log.info( 'validating job' )
   
     try :
-      db = be.share.sys.conf.get( 'device_db', section = 'job' )
+      db = sca3s_be.share.sys.conf.get( 'device_db', section = 'job' )
     
       if ( manifest.has( 'device_id' ) ) :
         t =  manifest.get( 'device_id' )
@@ -32,27 +32,27 @@ def process( manifest ) :
             manifest.put( key, value )
 
         else :
-          raise be.share.exception.ConfigurationException()
+          raise sca3s_be.share.exception.ConfigurationException()
     
-      share.share.schema.validate( manifest, task_share.schema.SCHEMA_MANIFEST )
+      sca3s_mw.share.schema.validate( manifest, task_mw.schema.SCHEMA_MANIFEST )
   
     except Exception as e :
       result = STATUS_FAILURE_VALIDATING_JOB ; raise e
 
-    be.share.sys.log.info( 'allocating job' )
+    sca3s_be.share.sys.log.info( 'allocating job' )
 
     try :
       id   = manifest.get( 'job_id' ) ; 
 
-      path = tempfile.mkdtemp( prefix = id + '.', dir = be.share.sys.conf.get( 'job', section = 'path' ) )
-      log  = be.share.log.build_log( be.share.log.TYPE_JOB, path = path, id = id, replace = { path : '${JOB}', os.path.basename( path ) : '${JOB}' } )
+      path = tempfile.mkdtemp( prefix = id + '.', dir = sca3s_be.share.sys.conf.get( 'job', section = 'path' ) )
+      log  = sca3s_be.share.log.build_log( sca3s_be.share.log.TYPE_JOB, path = path, id = id, replace = { path : '${JOB}', os.path.basename( path ) : '${JOB}' } )
 
-      job  = task_be.job.JobImp( manifest, path, log )
+      job  = task_sca3s_be.job.JobImp( manifest, path, log )
 
     except Exception as e :
       result = STATUS_FAILURE_ALLOCATING_JOB ; raise e
 
-    be.share.sys.log.info( 'processing job' )
+    sca3s_be.share.sys.log.info( 'processing job' )
 
     try :    
       job.process_prologue()
@@ -64,29 +64,29 @@ def process( manifest ) :
     finally :
       job.process_epilogue()
 
-    if ( be.share.sys.conf.get( 'clean', section = 'job' ) ) :
+    if ( sca3s_be.share.sys.conf.get( 'clean', section = 'job' ) ) :
       shutil.rmtree( path, ignore_errors = True )
 
   except Exception as e :
-    be.share.exception.dump( e, log = be.share.sys.log )
+    sca3s_be.share.exception.dump( e, log = sca3s_be.share.sys.log )
 
   return ( id, result )
 
 def run_mode_cli() :
-  if   ( be.share.sys.conf.has( 'manifest_file', section = 'job' ) ) :
-    manifest = be.share.conf.Conf( conf = be.share.sys.conf.get( 'manifest_file', section = 'job' ) )
-  elif ( be.share.sys.conf.has( 'manifest_data', section = 'job' ) ) :
-    manifest = be.share.conf.Conf( conf = be.share.sys.conf.get( 'manifest_data', section = 'job' ) )
+  if   ( sca3s_be.share.sys.conf.has( 'manifest_file', section = 'job' ) ) :
+    manifest = sca3s_be.share.conf.Conf( conf = sca3s_be.share.sys.conf.get( 'manifest_file', section = 'job' ) )
+  elif ( sca3s_be.share.sys.conf.has( 'manifest_data', section = 'job' ) ) :
+    manifest = sca3s_be.share.conf.Conf( conf = sca3s_be.share.sys.conf.get( 'manifest_data', section = 'job' ) )
   else :
-    raise be.share.exception.ConfigurationException()
+    raise sca3s_be.share.exception.BackEndException( message = sca3s_mw.share.status.INVALID_CONF )
 
   process( manifest )
 
 def run_mode_api() :
-  api      = task_be.api.APIImp()
+  api      = task_sca3s_be.api.APIImp()
       
-  api_wait = int( be.share.sys.conf.get( 'wait', section = 'api' ) )
-  api_ping = int( be.share.sys.conf.get( 'ping', section = 'api' ) )
+  api_wait = int( sca3s_be.share.sys.conf.get( 'wait', section = 'api' ) )
+  api_ping = int( sca3s_be.share.sys.conf.get( 'ping', section = 'api' ) )
 
   ping = 0 ; term = False 
 
@@ -111,50 +111,50 @@ def run_mode_api() :
         ping += 1
 
         if ( ( ping % api_ping ) == 0 ) :
-          be.share.sys.log.info( 'polled queue %d times ... no jobs', api_ping ) 
+          sca3s_be.share.sys.log.info( 'polled queue %d times ... no jobs', api_ping ) 
       
       else :  
         ping  = 0
 
-        ( id, result ) = process( be.share.conf.Conf( conf = manifest ) )
+        ( id, result ) = process( sca3s_be.share.conf.Conf( conf = manifest ) )
 
         if ( id != None ) :
           if   ( result == STATUS_SUCCESS                ) :
-            api.complete_job( id, error_code = task_be.api.JSONStatus.SUCCESS                )
+            api.complete_job( id, error_code = task_sca3s_be.api.JSONStatus.SUCCESS                )
           elif ( result == STATUS_FAILURE_VALIDATING_JOB ) :
-            api.complete_job( id, error_code = task_be.api.JSONStatus.FAILURE_VALIDATING_JOB )
+            api.complete_job( id, error_code = task_sca3s_be.api.JSONStatus.FAILURE_VALIDATING_JOB )
           elif ( result == STATUS_FAILURE_ALLOCATING_JOB ) :
-            api.complete_job( id, error_code = task_be.api.JSONStatus.FAILURE_ALLOCATING_JOB )
+            api.complete_job( id, error_code = task_sca3s_be.api.JSONStatus.FAILURE_ALLOCATING_JOB )
           elif ( result == STATUS_FAILURE_PROCESSING_JOB ) :
-            api.complete_job( id, error_code = task_be.api.JSONStatus.FAILURE_PROCESSING_JOB )
+            api.complete_job( id, error_code = task_sca3s_be.api.JSONStatus.FAILURE_PROCESSING_JOB )
   
       if ( term ) :
-        be.share.sys.log.info( 'handled SIGABRT or SIGTERM: terminating' ) ; return
+        sca3s_be.share.sys.log.info( 'handled SIGABRT or SIGTERM: terminating' ) ; return
   
       time.sleep( api_wait )
 
     except Exception as e :
-      be.share.exception.dump( e, log = be.share.sys.log )
+      sca3s_be.share.exception.dump( e, log = sca3s_be.share.sys.log )
 
 if ( __name__ == '__main__' ) :
   try :
-    be.share.sys.init()
+    sca3s_be.share.sys.init()
 
-    if   ( be.share.sys.conf.get( 'task', section = 'sys' ) == 'acquire' ) :
-      task_be    = importlib.import_module( 'sca3s.backend.acquire' )
-      task_share = importlib.import_module( 'sca3s.share.acquire' )
-    elif ( be.share.sys.conf.get( 'task', section = 'sys' ) == 'analyse' ) :
-      task_be    = importlib.import_module( 'sca3s.backend.analyse' )
-      task_share = importlib.import_module( 'sca3s.share.analyse' )
+    if   ( sca3s_be.share.sys.conf.get( 'task', section = 'sys' ) == 'acquire' ) :
+      task_be = importlib.import_module( 'sca3s.backend.acquire' )
+      task_mw = importlib.import_module( 'sca3s.middleware.acquire' )
+    elif ( sca3s_be.share.sys.conf.get( 'task', section = 'sys' ) == 'analyse' ) :
+      task_be = importlib.import_module( 'sca3s.backend.analyse' )
+      task_mw = importlib.import_module( 'sca3s.middleware.analyse' )
     else :
-      raise be.share.exception.ConfigurationException()
+      raise sca3s_be.share.exception.BackEndException( message = sca3s_mw.share.status.INVALID_CONF )
 
-    if   ( be.share.sys.conf.get( 'mode', section = 'sys' ) == 'cli'     ) :
+    if   ( sca3s_be.share.sys.conf.get( 'mode', section = 'sys' ) == 'cli'     ) :
       run_mode_cli()
-    elif ( be.share.sys.conf.get( 'mode', section = 'sys' ) == 'api'     ) :
+    elif ( sca3s_be.share.sys.conf.get( 'mode', section = 'sys' ) == 'api'     ) :
       run_mode_api()
     else :
-      raise be.share.exception.ConfigurationException()
+      raise sca3s_be.share.exception.BackEndException( message = sca3s_mw.share.status.INVALID_CONF )
 
   except Exception as e :
     raise e
