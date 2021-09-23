57e6ca976f0060c003301515d19e2c880370b0e0
Dan Page <dan@phoo.org>
2021-09-23 22:42:02 +0100

    bump

--- src/sca3s/backend/share/util.py
+++ src/sca3s/backend/share/util.py
@@ -148,8 +148,9 @@ def hdf5_set_data( spec, trace_content, fd, n, i, trace ) :
 
       sca3s_be.share.sys.log.debug( '!!! trace = %s' % ( str( trace ) ) )
  
-      sca3s_be.share.sys.log.debug( '!!!     trace[data/%s]  = %s' % ( k, str(      trace[ 'data/%s' % k   ) ) ) )
-      sca3s_be.share.sys.log.debug( '!!! len(trace[data/%s]) = %d' % ( k, str( len( trace[ 'data/%s' % k ) ) ) ) )
+      sca3s_be.share.sys.log.debug( '!!!     trace[data/%s]  = %s' % ( k, str(      trace[ 'data/%s' % k ]   ) ) )
+      sca3s_be.share.sys.log.debug( '!!! len(trace[data/%s]) = %d' % ( k, str( len( trace[ 'data/%s' % k ] ) ) ) )
+
       t = f( trace )
 
       sca3s_be.share.sys.log.debug( '!!! t = %s' % ( str( t ) ) )
